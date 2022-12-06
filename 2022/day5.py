7ef9c43273f038073231f156ae5d22f5075c174d
Alexander Carvalho <lagercarvalho@gmail.com>
2022-12-06 19:19:04 +0100

    day 5 done

--- 2022/day5.py
+++ 2022/day5.py
@@ -15,11 +15,13 @@ str1 = [
 def main():
     with open ("inputs/input5.txt", "r") as file:
         str = file.read().splitlines()
+    print("Part 1:")
     partOne(str)
-    #partTwo(str)
+    print("Part 2:")
+    partTwo(str)
 
 def partOne(str):
-    stack = [[] for x in range(9)]
+    stack = [[] for _ in range(9)]
     for s in str:
         if len(s) == 0:
             for x in range(len(stack)):
@@ -40,9 +42,30 @@ def partOne(str):
         print(s.pop())
 
 def partTwo(str):
+    stack = [[] for _ in range(9)]
     for s in str:
-        print(s)
-    print(sum)
+        if len(s) == 0:
+            for x in range(len(stack)):
+                stack[x].reverse()
+            continue
+        elif len(s) == 35:
+            l = [s[i * 4:(i + 1) * 4] for i in range((len(s) + 4 - 1) // 4 )]
+            for index, x in enumerate(l):
+                if x[0] == '[' :
+                    stack[index].append(x[1])
+        else:
+            num, send, rec = [int(l) for l in s.split() if l.isdigit()]
+            crates = []
+            for x in range(num):
+                if len(stack[send-1]) != 0:
+                    crates.append(stack[send-1].pop())
+            crates.reverse()
+            for crate in crates:
+                stack[rec-1].append(crate)
+
+
+    for s in stack:
+        print(s.pop())
 
 if __name__ == "__main__":
     main()
