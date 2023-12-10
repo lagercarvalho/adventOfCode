9b9139a4db961f51d2f6d7a484c0fc136d16ec09
Alexander Carvalho <lagercarvalho@gmail.com>
2023-12-10 21:50:15 +0100

    part1 of day3 done

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,14 +1,44 @@
 
 def main():
-  f = open("inputs\input2.txt") 
+  with open ("inputs/input3.txt", "r") as file:
+        input = file.read().splitlines()
   test1 = ["467..114..","...*......","..35..633.","......#...","617*......",".....+.58.","..592.....","......755.","...$.*....",".664.598.."]
 
-  part1(test1)
+  part1(input)
 
 def part1(input):
   matrix_input = [[char for char in line] for line in input]
+  sum = 0
 
-  for 
+  for line_index, line in enumerate(input):
+    number = []
+    index_start = -1
+    for char_index, char in enumerate(line):
+      if char.isdigit():
+        number.append(str(char))
+        if index_start == -1:
+          index_start = char_index
+
+      if (char_index == len(line) - 1 or not char.isdigit()) and index_start != -1: 
+        num = ''.join(digit for digit in number)
+        
+        if isPartsNum(matrix_input, line_index, index_start, char_index - 1):
+          sum += int(num)
+        
+        index_start = -1
+        number.clear()
+  print('Sum:', sum)
+      
+
+def isPartsNum(matrix, line_index, index_start, index_end):
+  for col in range(index_start - 1, index_end + 2):
+    if col < 0 or col >= len(matrix[line_index]): continue
+    for row in range(line_index - 1, line_index + 2):
+      if row < 0 or row >= len(matrix): continue
+      if not matrix[row][col].isdigit() and  matrix[row][col] != '.':
+        return True
+  return False
+
         
 
 if __name__ == "__main__":
